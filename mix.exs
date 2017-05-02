1c10cbfe5788f7a42b9f00f99d278991149213c7
Stephen M. Pallen <smpallen99@yahoo.com>
2017-05-02 15:28:58 -0400

    updated mix and deps

--- mix.exs
+++ mix.exs
@@ -17,25 +17,33 @@ defmodule UcxUcc.Mixfile do
   # Type `mix help compile.app` for more information.
   def application do
     [mod: {UcxUcc.Application, []},
-     extra_applications: [:logger, :runtime_tools]]
+     extra_applications: extra_applications(Mix.env)]
   end
+  defp extra_applications(:prod), do: [:logger, :runtime_tools]
+  defp extra_applications(_), do: extra_applications(:prod) ++ [:faker_elixir_octopus]
 
   # Specifies which paths to compile per environment.
-  defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(_),     do: ["lib"]
+  defp elixirc_paths(:test), do: ["plugins", "lib", "test/support"]
+  defp elixirc_paths(_),     do: ["plugins", "lib"]
 
   # Specifies your project dependencies.
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.3.0-rc"},
-     {:phoenix_pubsub, "~> 1.0"},
-     {:phoenix_ecto, "~> 3.2"},
-     {:mariaex, ">= 0.0.0"},
-     {:phoenix_html, "~> 2.6"},
-     {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+    [
+      {:phoenix, github: "phoenixframework/phoenix", override: true},
+      # {:phoenix, "~> 1.3.0-rc"},
+      {:phoenix_pubsub, "~> 1.0"},
+      {:phoenix_ecto, "~> 3.2"},
+      {:mariaex, ">= 0.0.0"},
+      {:phoenix_html, "~> 2.6"},
+      {:phoenix_live_reload, "~> 1.0", only: :dev},
+      {:gettext, "~> 0.11"},
+      {:unbrella, path: "../unbrella"},
+      {:coherence, github: "smpallen99/coherence", branch: "phx-1.3"},
+      {:faker_elixir_octopus, "~> 1.0", only: [:dev, :test]},
+      {:cowboy, "~> 1.0"}
+    ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
@@ -45,8 +53,8 @@ defmodule UcxUcc.Mixfile do
   #
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
-    ["ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+    ["ecto.setup": ["ecto.create", "unbrella.migrate", "unbrella.seed"],
      "ecto.reset": ["ecto.drop", "ecto.setup"],
-     "test": ["ecto.create --quiet", "ecto.migrate", "test"]]
+     "test": ["ecto.create --quiet", "unbrella.migrate", "test"]]
   end
 end
