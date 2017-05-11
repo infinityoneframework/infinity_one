f36ea5b69a9ffe6a5ed1efa1a49e7080b74dc9ca
Stephen M. Pallen <smpallen99@yahoo.com>
2017-05-10 21:51:09 -0400

    added ucc_chat plugin config

---
+++ plugins/ucc_chat/config/config.exs
@@ -0,0 +1,66 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+use Mix.Config
+
+config :unbrella, :plugins, ucc_chat: [
+  module: UccChat,
+  schemas: [UccChat.Accounts.Account],
+  router: UccChat.Web.Router,
+  page_size: 150,
+  defer: true,
+  emoji_one: [
+    # single_class: "big",
+    ascii: true,
+    wrapper: :span,
+    id_class: "emojione-"
+    # src_path: "/images",
+    # src_version: "?v=2.2.7",
+    # img_type: ".png"
+  ] 
+
+] 
+
+config :ucx_ucc, UccChat.Robot, [
+  # adapter: Hedwig.Adapters.Console,
+  adapter: UccChat.Robot.Adapters.UccChat,
+  name: "bot",
+  aka: "/",
+  responders: [
+    {Hedwig.Responders.Help, []},
+    {Hedwig.Responders.Ping, []},
+    {UccChat.Robot.Responders.Hello, []},
+
+    {HedwigSimpleResponders.Slogan, []},
+    {HedwigSimpleResponders.ShipIt, %{ extra_squirrels: false }},
+    {HedwigSimpleResponders.Time, []},
+    {HedwigSimpleResponders.Uptime, []},
+    {HedwigSimpleResponders.BeerMe, []},
+    {HedwigSimpleResponders.Fishpun, []},
+    {HedwigSimpleResponders.Slime, []},
+    {HedwigSimpleResponders.Slogan, []},
+  ]
+]
+
+# General application configuration
+# config :ucc_chat,
+#   ecto_repos: [UccChat.Repo]
+
+# # Configures the endpoint
+# config :ucc_chat, UccChat.Web.Endpoint,
+#   url: [host: "localhost"],
+#   secret_key_base: "6t0nS1VfAfJly5M6DwTo4u9XNXvbfJulh69nHEbLw7uTFfyCyBEfVGkRw1iEA7eo",
+#   render_errors: [view: UccChat.Web.ErrorView, accepts: ~w(html json)],
+#   pubsub: [name: UccChat.PubSub,
+#            adapter: Phoenix.PubSub.PG2]
+
+# # Configures Elixir's Logger
+# config :logger, :console,
+#   format: "$time $metadata[$level] $message\n",
+#   metadata: [:request_id]
+
+# # Import environment specific config. This must remain at the bottom
+# # of this file so it overrides the configuration defined above.
+# import_config "#{Mix.env}.exs"
