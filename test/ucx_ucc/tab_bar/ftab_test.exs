b8374d8d25f73095f6c86b95ce253c45518496a2
Stephen M. Pallen <smpallen99@yahoo.com>
2017-08-06 06:44:30 -0400

    tabbar remember open views

--- test/ucx_ucc/tab_bar/ftab_test.exs
+++ test/ucx_ucc/tab_bar/ftab_test.exs
@@ -3,11 +3,19 @@ defmodule UcxUcc.TabBar.FtabTest do
   doctest UcxUcc.TabBar.Ftab
 
   alias UcxUcc.TabBar
-  # alias TabBar.Ftab
+  alias TabBar.Ftab
 
   setup do
     TabBar.delete_all
     :ok
+  end
+
+  test "opens previous view" do
+    Ftab.open 1, 1, "test", %{name: "A"}
+    assert Ftab.open?(1, 1, "test")
+    Ftab.open 1, 1, "other", nil
+    Ftab.open 1, 1, "test", nil
+    assert Ftab.get(1, 1) == {"test", %{name: "A"}}
   end
 
 end
